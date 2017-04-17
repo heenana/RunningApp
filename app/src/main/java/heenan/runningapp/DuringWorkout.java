54b3a270a9508e9a57cc48fbedb599dc950deaca
gabrielgomoreira <gabrielgo.moreira@gmail.com>
2017-04-16 22:08:25 -0500

    Our GPS class works yaaaaaaay

--- app/src/main/java/heenan/runningapp/DuringWorkout.java
+++ app/src/main/java/heenan/runningapp/DuringWorkout.java
@@ -1,12 +1,19 @@
 package heenan.runningapp;
 
 import android.app.Activity;
+import android.app.Service;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.IBinder;
 import android.os.Vibrator;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -28,6 +35,10 @@ import static java.lang.Thread.sleep;
  */
 
 public class DuringWorkout  extends AppCompatActivity {
+
+    // GPS Variables
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     private TextView[] timer_views;
     private double[] day_data;
@@ -90,6 +101,23 @@ public class DuringWorkout  extends AppCompatActivity {
         walking.setText("Walk: " + walk + " minutes");
         sets_num.setText("Sets: " + (int) sets);
         total_length.setText("Total Workout Time: " + (int) length + " minutes");
+
+
+        Log.e("GPS ENABLES?", "ABOUT TO CHECK");
+        GPSTracker gps = new GPSTracker(this);
+        if(gps.canGetLocation()){
+            Log.e("GPS IS ENABLED", "Thank God");
+
+            double latitude = gps.getLatitude(); // returns latitude
+            double longitude = gps.getLongitude(); // returns longitude
+
+            Log.e("LAT IS: ", ""+latitude );
+            Log.e("LON IS: ", ""+longitude );
+
+
+
+        } // gps enabled} // return boolean true/false
+
 
         timer_updater();
 
@@ -303,4 +331,218 @@ public class DuringWorkout  extends AppCompatActivity {
         }
     }
 
+    public class GPSTracker extends Service implements LocationListener {
+
+        private final Context mContext;
+
+        // flag for GPS status
+        boolean isGPSEnabled = false;
+
+        // flag for network status
+        boolean isNetworkEnabled = false;
+
+        // flag for GPS status
+        boolean canGetLocation = false;
+
+        Location location; // location
+        double latitude; // latitude
+        double longitude; // longitude
+
+        // The minimum distance to change Updates in meters
+        private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+
+        // The minimum time between updates in milliseconds
+        private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+
+        // Declaring a Location Manager
+        protected LocationManager locationManager;
+
+        public GPSTracker(Context context) {
+            this.mContext = context;
+            getLocation();
+        }
+
+        public Location getLocation() {
+            try {
+                locationManager = (LocationManager) mContext
+                        .getSystemService(LOCATION_SERVICE);
+
+                // getting GPS status
+                isGPSEnabled = locationManager
+                        .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+                // getting network status
+                isNetworkEnabled = locationManager
+                        .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+                if (!isGPSEnabled && !isNetworkEnabled) {
+                    // no network provider is enabled
+                } else {
+                    this.canGetLocation = true;
+                    // First get location from Network Provider
+                    if (isNetworkEnabled) {
+                        try {
+                            locationManager.requestLocationUpdates(
+                                    LocationManager.NETWORK_PROVIDER,
+                                    MIN_TIME_BW_UPDATES,
+                                    MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                        } catch (SecurityException e) {
+                            Log.e("PERMISSION_EXCEPTION","PERMISSION_NOT_GRANTED");
+                        }
+
+                        Log.d("Network", "Network");
+                        if (locationManager != null) {
+                            try {
+                                location = locationManager
+                                        .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                            } catch (SecurityException e) {
+                                Log.e("PERMISSION_EXCEPTION","PERMISSION_NOT_GRANTED");
+                            }
+
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                        }
+                    }
+                    // if GPS Enabled get lat/long using GPS Services
+                    if (isGPSEnabled) {
+                        if (location == null) {
+                            try {
+                                locationManager.requestLocationUpdates(
+                                        LocationManager.GPS_PROVIDER,
+                                        MIN_TIME_BW_UPDATES,
+                                        MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                            } catch (SecurityException e) {
+                                Log.e("PERMISSION_EXCEPTION","PERMISSION_NOT_GRANTED");
+                            }
+
+                            Log.d("GPS Enabled", "GPS Enabled");
+                            if (locationManager != null) {
+
+                                try {
+                                    location = locationManager
+                                            .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                                } catch (SecurityException e) {
+                                    Log.e("PERMISSION_EXCEPTION","PERMISSION_NOT_GRANTED");
+                                }
+
+                                if (location != null) {
+                                    latitude = location.getLatitude();
+                                    longitude = location.getLongitude();
+                                }
+                            }
+                        }
+                    }
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return location;
+        }
+
+        /**
+         * Stop using GPS listener
+         * Calling this function will stop using GPS in your app
+         * */
+        public void stopUsingGPS(){
+            if(locationManager != null){
+                try {
+                    locationManager.removeUpdates(GPSTracker.this);
+                } catch (SecurityException e) {
+                    Log.e("PERMISSION_EXCEPTION","PERMISSION_NOT_GRANTED");
+                }
+            }
+        }
+
+        /**
+         * Function to get latitude
+         * */
+        public double getLatitude(){
+            if(location != null){
+                latitude = location.getLatitude();
+            }
+
+            // return latitude
+            return latitude;
+        }
+
+        /**
+         * Function to get longitude
+         * */
+        public double getLongitude(){
+            if(location != null){
+                longitude = location.getLongitude();
+            }
+
+            // return longitude
+            return longitude;
+        }
+
+        /**
+         * Function to check GPS/wifi enabled
+         * @return boolean
+         * */
+        public boolean canGetLocation() {
+            return this.canGetLocation;
+        }
+
+        /**
+         * Function to show settings alert dialog
+         * On pressing Settings button will lauch Settings Options
+         * */
+        public void showSettingsAlert(){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+
+            // Setting Dialog Title
+            alertDialog.setTitle("GPS is settings");
+
+            // Setting Dialog Message
+            alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
+
+            // On pressing Settings button
+            alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog,int which) {
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    mContext.startActivity(intent);
+                }
+            });
+
+            // on pressing cancel button
+            alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+
+            // Showing Alert Message
+            alertDialog.show();
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public IBinder onBind(Intent arg0) {
+            return null;
+        }
+
+    }
 }
+
+
